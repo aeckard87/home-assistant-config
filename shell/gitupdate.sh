468e4abe3dfa16736123b3799d2cc0f23b899fbb
jazzyisj <hassio.jazzyisj@gmail.com>
2019-11-20 23:51:01 -0500

    Unavailable Sensor Added

--- shell/gitupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#hassio homeassistant check
-cd /config
-git add .
-git status
-#echo -n "Enter the Description for the Change: " [Minor Update]
-#read CHANGE_MSG
-git commit -m "Test Update"
-git push origin master
